72bfe1d8ebc73025fd36b70d6f7cfe5ac1721aa4
Iloke Obinna <obinnadanil@gmail.com>
2024-08-21 02:01:33 +0100

    favicon added and logout button position modified

--- src/main/java/obi/com/grademaster/config/AppConfig.java
+++ src/main/java/obi/com/grademaster/config/AppConfig.java
@@ -29,7 +29,7 @@ public class AppConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((request) -> request
-                .requestMatchers("/addAdmin","/registerStudent","/css/**","/registerStudent").permitAll()
+                .requestMatchers("/favicon.ico","/registerStudent","/css/**","/registerStudent").permitAll()
                         .anyRequest()
                         .authenticated())
 
